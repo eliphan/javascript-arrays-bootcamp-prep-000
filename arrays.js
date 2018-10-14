9fe048e12c154ebd374517500590afab0b7387e9
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 03:36:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array,element) {
-  return [...array, "element"]
+  return [..."element", array]
 }
 
 function destructivelyAddElementToEndOfArray (array,element) {
-  return array.unshift("element")
+  return array.push("element")
 }
 
 function addElementToEndOfArray (array,element) {
-  
+  return [...array, "element"]
 }
