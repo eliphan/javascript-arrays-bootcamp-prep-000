bc1218c23f78d2b1703d5c9937a0096ab075c4ef
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 03:50:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array,index) {
   return (array[index])
 }
+
+function destructiveluyRemoveElementFromBeginningOfArray (array,element) {
+  array.shift()
+  return array
+}
