1fbbdacb293b6f3cef590f9d59db861edef9d02e
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 03:59:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,4 +36,6 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   return array
 }
 
-function removeElementFromEndOfArray(array)
+function removeElementFromEndOfArray(array) {
+  array.length
+}
