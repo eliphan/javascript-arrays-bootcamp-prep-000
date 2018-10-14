54fd37176880313576f4b7c6de7890cd479a7276
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 03:28:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 var myArray = ["cat", "dog", "rabbit"]
-function addElementToBeginningOfArray(myArray,chicken) {
-  return function () {myArray.push("chicken")
-}
-   return myArray
