2cef98fca04b61a6f4134c26cc450e9324687b9c
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 03:43:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function addElementToEndOfArray (array,element) {
   return [...array, element]
 }
 
-function destructivelyAddElementToEndOfArray (array,element) {
-  array.push("shift")
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
   return array
 }
