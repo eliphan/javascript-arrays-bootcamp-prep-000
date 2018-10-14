6102605b3c6d83452768e52d67a97c47bbdca3e8
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 03:33:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray (array,element) {
   return [...array, "element"]
 }
+
+function destructivelyAddElementToEndOfArray (array,element) {
+  return array.push("element")
+}
